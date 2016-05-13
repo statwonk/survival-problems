8595db53917ef8abd18a4c88dee61701252be97c
statwonk <cpeter9@gmail.com>
2016-05-12 21:24:17 -0500

    Not real subscription data, boo

---
+++ knime-churn.R
@@ -0,0 +1,14 @@
+# https://www.knime.org/knime-applications/churn-analysis
+# data: http://www.iainpardoe.com/teaching/dsc433/data/Churn.xls
+library(readr)
+library(dplyr)
+library(survival)
+library(magrittr)
+
+read_csv("Churn.csv") %>%
+  tbl_df %>%
+  mutate(censor = Churn == 0) %>%
+  survfit(Surv(`Account Length`, censor) ~ 1, data = .) %T>%
+  # Looks too smooth to be real subscription data.
+  plot() %>%
+  summary(times = 100)
